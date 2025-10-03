b24538dea261fb3214c28ae74fd6729a47560482
Voldemar <addos123@yandex.ru>
2025-10-03 13:17:37 +0300

    Add Test with widget ElevatedButton

--- lib/test_widget/test_widget_screen.dart
+++ lib/test_widget/test_widget_screen.dart
@@ -6,11 +6,20 @@ class TestWidgetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text(
-          "Нилов Владимир Владимирович\nИКБО-06-22\n22И1745",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20, color: Colors.blue),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: null,
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.amber),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+            ),
+          ),
+          child: const Text(
+            "Нилов Владимир Владимирович\nИКБО-06-22\n22И1745",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, color: Colors.blue),
+          ),
         ),
       ),
     );
