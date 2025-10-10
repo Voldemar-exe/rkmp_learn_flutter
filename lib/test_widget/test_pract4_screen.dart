4084bdb05468e664cac2a37708489da1e91c3b6b
Voldemar <addos123@yandex.ru>
2025-10-10 17:18:27 +0300

    Testing Keys functionality

--- lib/test_widget/test_pract4_screen.dart
+++ lib/test_widget/test_pract4_screen.dart
@@ -29,3 +29,65 @@ class TestListingWidgetScreen extends StatelessWidget {
     );
   }
 }
+
+class DeletionScreen extends StatefulWidget {
+  const DeletionScreen({super.key});
+
+  @override
+  State<DeletionScreen> createState() => _DeletionScreenState();
+}
+class _DeletionScreenState extends State<DeletionScreen> {
+  List<String> items = ['А', 'Б', 'В', 'Г', 'Д'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return _StatefulItem(
+            key: ValueKey(items[index].hashCode),
+            name: items[index],
+            onDelete: () {
+              setState(() {
+                items.removeAt(index);
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+class _StatefulItem extends StatefulWidget {
+  final String name;
+  final VoidCallback onDelete;
+
+  const _StatefulItem({
+    super.key,
+    required this.name,
+    required this.onDelete,
+  });
+
+  @override
+  State<_StatefulItem> createState() => _StatefulItemState();
+}
+class _StatefulItemState extends State<_StatefulItem> {
+  bool _isHighlighted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      key: widget.key,
+      title: Text(widget.name),
+      tileColor: _isHighlighted ? Colors.red[200] : null,
+      leading: Icon(_isHighlighted ? Icons.star : Icons.person),
+      onTap: widget.onDelete,
+      onLongPress: () {
+        setState(() {
+          _isHighlighted = true;
+        });
+      },
+    );
+  }
+}
