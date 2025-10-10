6acb44404cd842a21f13c1737cf04209325d4ab6
Voldemar <addos123@yandex.ru>
2025-10-10 15:19:42 +0300

    Testing ListView functionality

--- lib/test_widget/test_pract4_screen.dart
+++ lib/test_widget/test_pract4_screen.dart
@@ -7,16 +7,24 @@ class TestListingWidgetScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (int i = 1; i <= 50; i++) ...[
-              Text("Нилов Владимир Владимирович"),
-              Text("ИКБО-06-22"),
-              Text("22И1745"),
-            ],
-          ],
+        child: ListView.separated(
+          itemCount: 50,
+          itemBuilder: (context, index) {
+            final number = index + 1;
+            return Column(
+              children: [
+                Text("$number"),
+                Text("Нилов Владимир Владимирович"),
+                Text("ИКБО-06-22"),
+                Text("22И1745"),
+              ],
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
         ),
+
       ),
     );
   }
